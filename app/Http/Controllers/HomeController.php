564ff15b75a7e6f7304208eb94865051e5691675
ChrisLepko <57542929+ChrisLepko@users.noreply.github.com>
2020-01-18 01:01:24 +0100

    coomit xx api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\json_decode;
+use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
     /**
@@ -22,5 +26,29 @@ class HomeController extends Controller
     public function index()
     {
         return view('dashboard');
+    }
+
+    public function getCompany(Request $request){
+        $publicKey = 'pk_8936f153936f4ee3b77122727d80bead';
+        $client = new Client();
+
+        $name = $request->input('name');
+
+        $nameEndpoint = "https://cloud.iexapis.com/stable/stock/"
+        . $name . "/batch?types=quote,news,chart&range=1m&last=10&"
+        . "token=" . $publicKey;
+
+        $nameResponse = $client->request('GET', $nameEndpoint);
+        $nameResponseBody = $nameResponse->getBody();
+        $nameResponseJSON = json_decode($nameResponseBody);
+
+        $chart = $nameResponseJSON->chart[0];
+        
+
+        $result = view('dashboard')
+            -> with('chart', $chart);
+
+        return $result;
+
     }
 }
